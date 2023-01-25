2ae9a84bf10b01b0ab5c7cc94ee1b6445aecae92
Naman Sharma <96349715+namansharma3007@users.noreply.github.com>
2023-01-25 22:25:09 +0530

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed("#autoType", {
-    strings:["I do caffeine coding", "My cycle is \"Coffee Break Code\"", "Sometimes I go in Code Coma", "I need coffee naps", "Code Fuel == Coffee", "Coffee-Driven Development is my only motivation", "Power Nap == Coding", "I have debugging dreams"],
+    strings:["I do caffeine coding", "My cycle is Coffee-Break-Code", "Sometimes I go in Code Coma", "I need coffee naps", "Code Fuel == Coffee", "Coffee-Driven Development is my only motivation", "Power Nap == Coding", "I have debugging dreams"],
     typeSpeed: 100,
     backSpeed: 100,
     loop: true
